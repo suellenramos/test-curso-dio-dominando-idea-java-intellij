b89befba384a3331c8188d4411aee0722d004cf5
Suellen Barros <suellen.barrosramos@gmail.com>
2022-05-08 22:55:24 -0400

    Primeiro Cmmit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Resultado: " + (a + b));*/
+    }
+}
