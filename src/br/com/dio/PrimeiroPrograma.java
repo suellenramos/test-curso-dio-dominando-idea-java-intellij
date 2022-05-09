dbf231ea45b4d3eeccbbac9929331cbfe6aef0af
Suellen Barros <suellen.barrosramos@gmail.com>
2022-05-08 23:19:09 -0400

    Criando Classe Livro.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,11 +6,48 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Cachorro cachorro = new Cachorro();
-
         System.out.println(cachorro);
+
+        Livro livro = new Livro("Livro Programação na Pática", 300 );
+        System.out.println(livro);
 
        /* int a = 5;
         int b = 3;
         System.out.println("Resultado: " + (a + b));*/
+    }
+}
+
+class Livro{
+
+    private String nome;
+    private Integer numPage;
+
+    public Livro(String nome, Integer numPage) {
+        this.nome = nome;
+        this.numPage = numPage;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(Integer numPage) {
+        this.numPage = numPage;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPage=" + numPage +
+                '}';
     }
 }
